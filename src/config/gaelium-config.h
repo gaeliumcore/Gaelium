7bf87466cdd998567651c13ed6abd2ec451a439d
Gaelium Core <contact@gaelium.io>
2026-08-12 17:49:39 +0000

    chore: bump version to 4.6.1.1

--- src/config/gaelium-config.h
+++ src/config/gaelium-config.h
@@ -9,7 +9,7 @@
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Version Build */
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 1
 
 /* Version is release */
 #define CLIENT_VERSION_IS_RELEASE true
@@ -344,7 +344,7 @@
 #define PACKAGE_NAME "Gaelium Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Gaelium Core 4.6.1"
+#define PACKAGE_STRING "Gaelium Core 4.6.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gaelium"
@@ -353,7 +353,7 @@
 #define PACKAGE_URL "https://gaelium.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "4.6.1"
+#define PACKAGE_VERSION "4.6.1.1"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
